d9626a5e3a38c6c510e2b68172023a8af44e7447
SiliconMage <dansub0@gmail.com>
2025-03-10 14:38:45 -0400

    Updated to v1.4.4
    -Fixed an issue with audio being played at full volume whether the game lost focus or not
    -Made it easier to modify companion stats when they have special characters in their name, namely Eder.

---
+++ Mods/VolumeMods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityModManagerNet;
+
+namespace IEMod.Mods
+{
+    internal class VolumeMods
+    {
+        [HarmonyPatch(typeof(VolumeAsCategory))]
+        [HarmonyPatch("UpdateVolume")]
+        static class VolumeAsCategory_UpdateVolumn_Patch
+        {
+            static void Postfix(VolumeAsCategory __instance)
+            {
+                if (ModMain.Settings.PlayAudioWhenWindowLosesFocus)
+                {
+                    var self = Traverse.Create(__instance);
+
+
+                    if (__instance.Source && !self.Field<bool>("m_hasFocus").Value)
+                    {
+                        __instance.Source.volume = __instance.ExternalVolume * GameState.Option.GetVolume(__instance.Category);
+                    }
+                }
+            }
+        }
+    }
+}
